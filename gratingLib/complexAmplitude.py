50149eebc71b1352a28ccef535637f7c9d74396f
haiimjoee <jarroyo3@hawk.iit.edu>
2017-11-10 10:53:14 -0600

    Added phase to complex amplitude

--- gratingLib/complexAmplitude.py
+++ gratingLib/complexAmplitude.py
@@ -1,3 +1,5 @@
-def complexAmplitude(U_0,k,r):
-    U = U_0 * (math.cos(k*r) + 1j*math.sin(k*r))/r
+import numpy as np
+
+def complexAmplitude(U_0,k,r,phase):
+    U = U_0* np.exp(1j*phase) * (np.cos(k*r) + 1j*np.sin(k*r))/r
     return U
